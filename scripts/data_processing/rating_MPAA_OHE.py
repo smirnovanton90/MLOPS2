77c054fe4841df13511833c883c106c5653f1e12
ml_srv_admin <smirnovanton90@gmail.com>
2023-10-08 15:22:02 +0300

    Add stage 3 and changing stage 1

--- scripts/data_processing/rating_MPAA_OHE.py
+++ scripts/data_processing/rating_MPAA_OHE.py
@@ -3,19 +3,20 @@ import os
 import io
 import pandas as pd
 
-
 if len(sys.argv) != 2:
     sys.stderr.write("Arguments error. Usage:\n")
-    sys.stderr.write("\tpython3 get_features.py data-file\n")
+    sys.stderr.write("\tpython3 change_text_to_numeric.py data-file\n")
     sys.exit(1)
 
 f_input = sys.argv[1]
-f_output = os.path.join("datasets", "stage1", "train.csv")
-os.makedirs(os.path.join("datasets", "stage1"), exist_ok=True)
+f_output = os.path.join("datasets", "stage3", "train.csv")
+os.makedirs(os.path.join("datasets", "stage3"), exist_ok=True)
 
 def process_data(fd_in, fd_out):
     data = pd.read_csv(fd_in)
-    data_prepared = data[['id', 'rating_MPAA', 'director', 'rating']]
+    one_hot = pd.get_dummies(data['rating_MPAA'])
+    data_prepared = data.drop('rating_MPAA', axis = 1)
+    data_prepared = data_prepared.join(one_hot)
     fd_out.write(data_prepared.to_csv(index = False))
 
 with io.open(f_input, encoding="utf8") as fd_in:
